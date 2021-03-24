4cdc3b810e1b5bb2ce5404cfbd51399f0a07749e
Vitor Hugo Risso da Silva <CIANDT\vitorhr@lnb028042cps.ciandt.global>
2021-03-24 14:42:02 -0300

    :tada:

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -17,6 +17,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -32,12 +33,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Find the Button in the layout
         val rollButton: Button = findViewById(R.id.button)
-
-        // Set a click listener on the button to roll the dice when the user taps the button
         rollButton.setOnClickListener { rollDice() }
+        rollDice()
     }
 
     /**
@@ -47,10 +45,32 @@ class MainActivity : AppCompatActivity() {
         // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.dice)
 
-        // Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        var drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.ic_launcher_background
+        }
+
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
+
+//        with(diceImage) {
+//            when (diceRoll) {
+//                1 -> this.setImageResource(R.drawable.dice_1)
+//                2 -> this.setImageResource(R.drawable.dice_2)
+//                3 -> this.setImageResource(R.drawable.dice_3)
+//                4 -> this.setImageResource(R.drawable.dice_4)
+//                5 -> this.setImageResource(R.drawable.dice_5)
+//                6 -> this.setImageResource(R.drawable.dice_6)
+//            }
+//        }
+
     }
 }
 
